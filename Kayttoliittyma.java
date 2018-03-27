43ff874d51f21c1cf7495d6a6327282fcc51afc6
Tapio Nevalainen <Tee@eduroam-238-239.uta.fi>
2018-03-27 15:26:41 +0300

    Lisäsin ehdotuksen kaupan logosta

--- Kayttoliittyma.java
+++ Kayttoliittyma.java
@@ -4,8 +4,17 @@ public class Kayttoliittyma{
 
 	public static void main(String[] args){
 
-		System.out.println("Testing");
-
+		System.out.println("       _      _       _                                  __  __");
+		
+		System.out.println("  /\\ /(_)_ __(_) __ _| | ____ _ _   _ _ __  _ __   __ _  \\ \\/ /");
+		
+		System.out.println(" / //_/ | '__| |/ _` | |/ / _` | | | | '_ \\| '_ \\ / _` |  \\  / ");
+		
+		System.out.println("/ __ \\| | |  | | (_| |   < (_| | |_| | |_) | |_) | (_| |  /  \\ ");
+		
+		System.out.println("\\/  \\/|_|_| _/ |\\__,_|_|\\_\\__,_|\\__,_| .__/| .__/ \\__,_| /_/\\_\\");
+		System.out.println("           |__/                      |_|   |_|                 ");
+		
 	}
 
 
