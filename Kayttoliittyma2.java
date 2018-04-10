47e20de1126d74a4c575360c09a4b15a4975ca19
Tapio Nevalainen <Tee@eduroam-237-240.uta.fi>
2018-04-10 16:43:26 +0300

    Lisää käyttöliittymän muokkausta. Toimiiko clear screen windows koneella?

--- Kayttoliittyma2.java
+++ Kayttoliittyma2.java
@@ -7,6 +7,66 @@ public class Kayttoliittyma2{
 
 		Scanner lukija = new Scanner(System.in);
 
+		// "Clear screen"
+		System.out.print("\033[H\033[2J");
+
+		printBanner();
+
+		System.out.println("\nTervetuloa kirjakauppaan!\nValitse toiminto:\n");
+
+		String syote = "";		
+
+		while(!syote.equals("4")){
+
+			System.out.println("[ 1 ] Kirjaudu sisään (Asiakas)");
+			System.out.println("[ 2 ] Kirjaudu sisään (Ylläpitäjä)");
+			System.out.println("[ 3 ] Rekisteröidy");
+			System.out.println("[ 4 ] Lopeta");
+			System.out.print("\n> ");
+
+			syote = lukija.nextLine();
+
+			if(!(syote.equals("1") || syote.equals("2") ||syote.equals("3") || syote.equals("4"))){
+				System.out.print("\033[H\033[2J");
+				printBanner();
+				System.out.println("Virheellinen syöte!\n");
+			}	
+
+			if(syote.equals("1")){
+				// "Clear screen"
+				System.out.print("\033[H\033[2J");
+				kirjaudu(lukija);
+
+			}else if(syote.equals("2")){
+				System.out.print("\033[H\033[2J");
+				kirjaudu(lukija);
+			}
+			
+			
+		}
+		printBanner();
+		System.out.print("\033[H\033[2J");
+		printBanner();
+		System.out.println("Tervetuloa uudelleen");
+
+
+		
+	}
+
+	public static boolean kirjaudu(Scanner lukija){
+		printBanner();
+		String username ="";
+		String password = "";
+		System.out.println("Kirjaudutaan sisään käyttäjänä.");
+		System.out.println("Syötä käyttäjätunnus: ");
+		username = lukija.nextLine();
+		System.out.println("Syötä salasana: ");
+		password = lukija.nextLine();
+
+		return true;
+	}
+
+	public static void printBanner(){
 		System.out.println("       _      _       _                                  __  __");
 		
 		System.out.println("  /\\ /(_)_ __(_) __ _| | ____ _ _   _ _ __  _ __   __ _  \\ \\/ /");
@@ -17,40 +77,6 @@ public class Kayttoliittyma2{
 		
 		System.out.println("\\/  \\/|_|_| _/ |\\__,_|_|\\_\\__,_|\\__,_| .__/| .__/ \\__,_| /_/\\_\\");
 		System.out.println("           |__/                      |_|   |_|                 ");
-
-		System.out.println("\nTervetuloa kirjakauppaan!\nValitse toiminto:\n");
-
-		String syote = "";		
-
-		while(!syote.equals("3")){
-
-			System.out.println("[ 1 ] Kirjaudu sisään (Asiakas)");
-			System.out.println("[ 2 ] Kirjaudu sisään (Ylläpitäjä)");
-			System.out.println("[ 3 ] Lopeta");
-			System.out.print("\n> ");
-
-			syote = lukija.nextLine();
-
-			if(!(syote.equals("1") || syote.equals("2") || syote.equals("3"))){
-				System.out.println("Virheellinen syöte!");
-			}
-
-			if(syote.equals("1")){
-				String username ="";
-				String password = "";
-				System.out.println("Kirjaudutaan sisään käyttäjänä.");
-				System.out.println("Syötä käyttäjätunnus: ");
-				username = lukija.nextLine();
-				System.out.println("Syötä salasana: ");
-				password = lukija.nextLine();
-
-			}
-			
-			
-		}
-
-
-		
 	}
 
 
