4a5f3dcdcc93420fa9327eb3cfa48ae066203481
Tapio Nevalainen <Tee@Phoenix.local>
2018-04-14 15:16:21 +0300

    kyselytesti

--- Kyselyt.java
+++ Kyselyt.java
@@ -1,26 +1,51 @@
 import java.sql.*;
+import java.util.HashMap;
 
 public class Kyselyt{
 
 	// Attribuutteina prepared statementtien merkkijonoesitykset
-	private Connection yhteys;
+	private Yhteys yhteys;
+	private Connection connection;
+	private ResultSet resultset;
 	private PreparedStatement preparedStatement;
 	private String haeKayttajaTiedot;
+	private HashMap<String, String> kyselyTulos;
 
 	// Kontruktori, asetetaan prepared statementtien rungot
-	public Kyselyt(Connection yhteys){
-		this.yhteys = yhteys;
+	public Kyselyt(){
+		this.yhteys = new Yhteys("localhost", 5432, "bookstore", "testuser", "12345");
+		this.connection = null;
+		this.resultset = null;
 		this.preparedStatement = null;
+		
 		this.haeKayttajaTiedot = "SELECT nimi,kayttajaid,salasana,rooli FROM kayttaja WHERE kayttajaid = ?";
 	}
 
 	// Metodi palauttaa rs.objektin
-	public ResultSet haeKayttajaTiedot(int id){
-		this.preparedStatement = this.yhteys.prepareStatement(this.haeKayttajaTiedot);
-		this.preparedStatement.setInt(1,id);
-		ResultSet rs = this.preparedStatement.executeQuery();
-		return rs;
+	public HashMap<String,String> haeKayttajaTiedot(int id){
+
+		this.kyselyTulos = new HashMap<String,String>();
+
+		try{
+			this.connection = this.yhteys.uusiYhteys();
+			this.preparedStatement = this.connection.prepareStatement(this.haeKayttajaTiedot);
+			this.preparedStatement.setInt(1,id);
+			this.resultset = this.preparedStatement.executeQuery();
+
+			while(this.resultset.next()){
+				this.kyselyTulos.put("nimi",this.resultset.getString("nimi"));
+				this.kyselyTulos.put("salasana",this.resultset.getString("salasana"));
+				this.kyselyTulos.put("rooli",this.resultset.getString("rooli"));
+			}
+			
+
+		}catch(SQLException poikkeus) {
+        	System.out.println("Tapahtui seuraava virhe: " + poikkeus.getMessage());  
+      	}
+      	return this.kyselyTulos;
+		
 	}
 
+	
 }
 
---
+++ Kyselytesti.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Kyselytesti{
+
+
+	public static void main(String[] args){
+
+		HashMap<String,String> kyselytulos = new HashMap<String,String>();
+		
+
+      	Kyselyt kysely = new Kyselyt();
+
+      	kyselytulos = kysely.haeKayttajaTiedot(1);
+
+      	System.out.println(kyselytulos.get("nimi"));
+      	System.out.println(kyselytulos.get("salasana"));
+      	System.out.println(kyselytulos.get("rooli"));
+
+
+	}
+
+
+
+
+
+
+}
