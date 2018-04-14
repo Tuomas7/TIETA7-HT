83fe0e36719a2f313447ccb1742accbe44cb5f09
Tuomas Tammela <tammela.tuomas@gmail.com>
2018-04-14 19:25:09 +0300

    Lisäsin tapahtumien 4 & 5 tilausvaiheen ja muutin ne yhteensopiviksi KayttajaTapahtumat-luokan kanssa

--- Tapahtumat.java
+++ Tapahtumat.java
@@ -1,5 +1,6 @@
 import java.sql.*;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Tapahtumat{
 
@@ -144,9 +145,9 @@ public class Tapahtumat{
    /* Tapahtumat 4 & 5 (varausvaihe)
     * Kuvaus: Varataan yksittäinen kappale ja lisätään se "ostoskoriin"
     * Rooli: Asiakas
-    * Parametrit: yhteys, tilausta tekevän asiakkaan ID, tilattavan kappaleen ID
+    * Parametrit: yhteys, tilausta tekevän asiakkaan sessio, tilattavan kappaleen ID
     */
-	public static void lisaaOstoskoriin(Connection yhteys, int asiakasID, int kappaleID) {
+	public static void lisaaOstoskoriin(Connection yhteys, Sessio sessio, int kappaleID) {
    
       try {
    
@@ -156,7 +157,7 @@ public class Tapahtumat{
          Statement stmt = yhteys.createStatement();
          
          // Haetaan kirjan omistavan divarin id 
-         ResultSet rset = stmt.executeQuery("SELECT DivariID FROM sijainti"
+         ResultSet rset = stmt.executeQuery("SELECT DivariID FROM keskus.sijainti"
             + "WHERE KappaleID=" + kappaleID);
          
          int divariID = rset.getInt(1);
@@ -182,7 +183,7 @@ public class Tapahtumat{
       
          // Luodaan uusi käynnissä oleva tilaus (vastaa kappaleen siirtämistä "ostoskoriin")
          stmt.executeUpdate("INSERT INTO keskus.Tilaus VALUES ('" + divariID + "', '"
-            + asiakasID + "', '" + kappaleID + "', 'Käynnissä')");
+            + sessio.id + "', '" + kappaleID + "', 'Käynnissä')");
       
          System.out.println("Kappale lisätty ostoskoriin.");
          
@@ -206,20 +207,139 @@ public class Tapahtumat{
             System.out.println("Tapahtuman peruutus epäonnistui: " + poikkeus2.getMessage()); 
          }
       }
-	}
+   }
    
    
    /* Tapahtumat 4 & 5 (tilausvaihe)
     * Kuvaus: Tilataan ostoskorissa olevat teoskappaleet
     * Rooli: Asiakas
-    * Parametrit: yhteys, tilausta tekevän asiakkaan ID
+    * Parametrit: yhteys, tilausta tekevän asiakkaan sessio
     */
-	public static void tilaaOstoskori(Connection yhteys, int asiakasID) {
+   public static void tilaaOstoskori(Connection yhteys, Sessio sessio) {
    
-      // Varsinainen tilaaminen tulee siis tähän
-	}
+      try {
    
+         yhteys.setAutoCommit(false);
    
-   // Tapahtumat 6-8
-   
+         // Luodaan tapahtumaolio
+         Statement stmt = yhteys.createStatement();
+         
+         // Haetaan "ostoskorin" sisältö
+         ResultSet rset = stmt.executeQuery("SELECT Teos.Nimi, Teos.Paino, TeosKappale.KappaleID "
+            + "FROM keskus.Tilaus NATURAL JOIN keskus.TeosKappale NATURAL JOIN keskus.Teos "
+            + "WHERE Tilaus.Tila='Käynnissä' AND Tilaus.AsiakasID=" + sessio.id);
+            
+         // Oskoskorissa olevien kappaleiden ID-arvot
+         Stack<Integer> teosKappaleet = new Stack<Integer>();
+            
+         // Kirjojen kokonaispaino
+         int kokoPaino = 0;
+            
+         System.out.println("Tilataan seuraavat kirjat:");
+         System.out.println();
+            
+         while (rset.next()) {
+           System.out.println(rset.getString(1));
+           teosKappaleet.push(rset.getInt(2));
+           kokoPaino += rset.getInt(3);
+         }
+         
+         // Tilauserien lukumäärä
+         int eraLkm = 1;
+         
+         // Lasketaan, moneenko erään tilaus täytyy jakaa (yksi erä on maksimissaan 2000 grammaa)
+         while (kokoPaino > 2000) {
+            eraLkm++;
+            kokoPaino -= 2000;
+         }
+         
+         if (eraLkm > 1) {
+            System.out.println();
+            System.out.println("Tilaus jaetaan painon vuoksi " + eraLkm + " erään.");
+         }
+         
+         // Postikulujen summa
+         float postikulut = 0;
+         
+         // Jokainen 2000 grammaa painava erä maksaa 14 euroa
+         postikulut += (eraLkm-1)*14.00;
+         
+         // Lasketaan yli menevän osan postikulut
+         if (kokoPaino <= 50) {
+            postikulut += 1.40;
+         }
+         else if (kokoPaino <= 100) {
+            postikulut += 2.10;
+         }
+         else if (kokoPaino <= 250) {
+            postikulut += 2.80;
+         }
+         else if (kokoPaino <= 500) {
+            postikulut += 5.60;
+         }
+         else if (kokoPaino <= 1000) {
+            postikulut += 8.40;
+         }
+         else {
+            postikulut += 14.00;
+         }
+         
+         System.out.println();
+         System.out.println("Tilauksen postikulut ovat " + postikulut + " euroa. Vahvistetaanko tilaus? (k/e)");
+         
+         // Otetaan asiakkaan valinta talteen
+         Scanner scanner = new Scanner(System.in);
+         char valinta = scanner.next().charAt(0);
+         
+         // Jatketaan, jos asiakas syöttää k:n. Kaikki muut syötteet peruvat tapahtuman
+         if (valinta == 'k' || valinta == 'K') {
+            
+            // Käydään läpi kaikki ostoskorissa olevat teoskappaleet
+            while (!(teosKappaleet.isEmpty())) {
+               
+               int kappaleID = teosKappaleet.pop();
+               
+               // Asetetaan tilaus suoritetuksi
+               stmt.executeUpdate("UPDATE keskus.Tilaus SET Tila='Suoritettu' WHERE KappaleID=" + kappaleID);
+               
+               // Asetetaan keskusdivarissa oleva kappale myydyksi
+               stmt.executeUpdate("UPDATE keskus.TeosKappale SET Vapaus='Myyty' WHERE KappaleID=" + kappaleID);
+               
+               // Haetaan kappaleen omistavan alkuperäisen divarin ID
+               ResultSet rset2 = stmt.executeQuery("SELECT DivariID FROM keskus.sijainti WHERE KappaleID=" + kappaleID);
+               int divariID = rset2.getInt(1);
+         
+               // Jos divari on jokin muu kuin keskustietokanta, asetetaan kappale myydyksi myös siellä
+               if (divariID != 2) {
+                  stmt.executeUpdate("UPDATE D" + divariID + ".TeosKappale SET Vapaus='Myyty' WHERE KappaleID=" + kappaleID);
+               }
+            }
+            
+            System.out.println("Tilaus suoritettu onnistuneesti!");
+         }
+         else {
+            System.out.println("Tilaus peruttu");
+         }
+         
+         // Sitoudutaan muutoksiin
+         yhteys.commit();
+         yhteys.setAutoCommit(true);
+         
+         // Suljetaan tapahtumaolio
+         stmt.close();
+         
+      } catch (SQLException poikkeus) {
+         
+         System.out.println("Tilaus epäonnistui: " + poikkeus.getMessage());  
+         
+         try {
+            
+            // Perutaan tapahtuma
+            yhteys.rollback();
+            
+         } catch (SQLException poikkeus2) {
+            System.out.println("Tapahtuman peruutus epäonnistui: " + poikkeus2.getMessage()); 
+         }
+      }
+   }
 }
